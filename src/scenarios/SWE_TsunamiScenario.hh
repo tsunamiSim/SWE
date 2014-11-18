4c55e40d06f562988457008f09836f549228cf08
Ludwig <ludwig.peuckert@googlemail.com>
2014-11-18 16:18:20 +0100

    implemented readNcFile function

--- src/scenarios/SWE_TsunamiScenario.hh
+++ src/scenarios/SWE_TsunamiScenario.hh
@@ -40,89 +40,105 @@
  * displacement in the middle of the domain  
  */
 class SWE_TsunamiScenario : public SWE_Scenario {
-    private: 
-    int *initX, *initY, *dispX, *dispY;
-    float **initZ, **dispZ;      
-    size_t init_ylen, init_xlen, dis_ylen, dis_xlen;
-    
-    void ba(int lookUpX, int lookUpY, int index, float &buff,int &squareDis){
+  private: 
+  Float2D *bathymetry, *displacement;
+  int *bathX, *bathY, *disX, *disY;	    
+
+  void lookUp(float x, float y){
                 /*float tmp = (lookUpX - initX[index]) * (lookUpX - initX[index]) + (lookUpY - initY[index]) * (lookUpY - initY[index]);
                 if(squareDis < 0 || squareDis > tmp){
                     buff = initZ[index]; 
                     squareDis = tmp;
                 }*/
-    };
+  };
 
-    void readBathymetry(){
-        int retval, ncid, dim, countVar, gats, unldim, zid, yid, xid;
-        char dimZ = 'z', dimY = 'y', dimX = 'x'; 
+	void readNcFile(const char* fileDir, Float2D* buffZ, int* buffY, int* buffX){
+		int retval, ncid, dim, countVar, 
+		zid = 2, yid = 1, xid = 0;
+		float **initZ;      
+		size_t init_ylen, init_xlen;
+		   
+		if(retval = nc_open(fileDir, NC_NOWRITE, &ncid)) ERR(retval);
 
-        if(retval = nc_open("NetCDF_Input/initBathymetry.nc", NC_NOWRITE, &ncid)) ERR(retval);
-	
-        if(retval = nc_inq(ncid, &dim, &countVar, &gats, &unldim)) ERR(retval);
-        assert(dim == 2); 
-        assert(countVar == 3); 
-        if(retval = nc_inq_varid(ncid, &dimZ, &zid)) ERR(retval);
-        if(retval = nc_inq_dimid(ncid, &dimY, &yid)) ERR(retval);
-        if(retval = nc_inq_dimid(ncid, &dimX, &xid)) ERR(retval); 
+		if(retval = nc_inq(ncid, &dim, &countVar, NULL, NULL)) ERR(retval);
+		assert(dim == 2); 
+		assert(countVar == 3); 
 
-        if(retval = nc_inq_dimlen(ncid, yid, &init_ylen)) ERR(retval);
-        if(retval = nc_inq_dimlen(ncid, xid, &init_xlen)) ERR(retval);
-	
-        initZ = new float*[init_xlen];
-        for(unsigned int i = 0; i < init_xlen; i++)
-            initZ[i] = new float[init_ylen];
+#ifndef NDBUG
+		char dimZ, dimY, dimX;
+		nc_type nc;
+		if(retval = nc_inq_dim(ncid, yid, &dimY, &init_ylen)) ERR(retval);
+		if(retval = nc_inq_dim(ncid, xid, &dimX, &init_xlen)) ERR(retval);
+		if(retval = nc_inq_var(ncid, zid, &dimZ, NULL, NULL, NULL, NULL)) ERR(retval);
+		
+		cout << "name dimZ: " << dimZ << endl;
+		cout << "name/length dimY: " << dimY << init_ylen << endl;
+		cout << "name/length dimX: " << dimX << init_xlen << endl;
+#else 
+		if(retval = nc_inq_dim(ncid, yid, NULL, &init_ylen)) ERR(retval);
+		if(retval = nc_inq_dim(ncid, xid, NULL, &init_xlen)) ERR(retval);
+#endif
 
-        initY = new int[init_ylen];
-        initX = new int[init_xlen];
-	
-        if(retval = nc_get_var_float(ncid, zid, &initZ[0][0])) ERR(retval);
-        if(retval = nc_get_var_int(ncid, yid, &initY[0])) ERR(retval);
-        if(retval = nc_get_var_int(ncid, xid, &initX[0])) ERR(retval);
-	   
-        if(retval = nc_close(ncid));  
-    };
+		initZ = new float*[init_xlen];
+    for(unsigned int i = 0; i < init_xlen; i++)
+        initZ[i] = new float[init_ylen];
+		cout << "1" <<endl;
+    buffY = new int[init_ylen];
+    buffX = new int[init_xlen];
 
-    void readDisplacement(){
-        
-    };
+		cout << "1" <<endl;
+    if(retval = nc_get_var_float(ncid, zid, &initZ[0][0])) ERR(retval);
+		cout << "1" <<endl;
+    if(retval = nc_get_var_int(ncid, yid, &buffY[0])) ERR(retval);
+		cout << "1" <<endl; 
+		if(retval = nc_get_var_int(ncid, xid, &buffX[0])) ERR(retval);
 
-  public:
+ 
+		cout << "1" <<endl;
+    if(retval = nc_close(ncid));
 
-    SWE_TsunamiScenario() : SWE_Scenario(){
-        readBathymetry();
-        readDisplacement();
+		cout << "1" <<endl;
+		buffZ = new Float2D(init_ylen, init_xlen, initZ);
+		assert(buffZ[10][15] = initZ[10][15]);
 
-        cout << "check" << " " << init_ylen << " " << init_xlen << " " << initZ[0][0] << " "<< initY[0] << " "<< initX[0] << " \n";
-    };
+  };
 
-    float getBathymetry(float x, float y) {
-	    return 0;
-    };
+public:
 
-    float getWaterHeight(float x, float y) { 
-       return 0;
-    };
+  SWE_TsunamiScenario() : SWE_Scenario(){
+      readNcFile("NetCDF_Input/initBathymetry.nc", bathymetry, bathY, bathX);
+			cout << "succesfully read initBathymetry" << endl;
+			readNcFile("NetCDF_Input/displacement.nc", displacement, disY, disX);   
+			cout << "succesfully read isplacement" << endl;     
+  };
 
-    virtual float endSimulation() { return (float) 30; };
+  float getBathymetry(float x, float y) {
+    return 0;
+  };
 
-    virtual BoundaryType getBoundaryType(BoundaryEdge edge) { return OUTFLOW; };
+  float getWaterHeight(float x, float y) { 
+     return 0;
+  };
 
-    /** Get the boundary positions
-     *
-     * @param i_edge which edge
-     * @return value in the corresponding dimension
-     */
-    float getBoundaryPos(BoundaryEdge i_edge) {
-       if ( i_edge == BND_LEFT )
-         return (float)-5000;
-       else if ( i_edge == BND_RIGHT)
-         return (float)5000;
-       else if ( i_edge == BND_BOTTOM )
-         return (float)-5000;
-       else
-         return (float)5000;
-    };
+  virtual float endSimulation() { return (float) 30; };
+
+  virtual BoundaryType getBoundaryType(BoundaryEdge edge) { return OUTFLOW; };
+
+  /** Get the boundary positions
+   *
+   * @param i_edge which edge
+   * @return value in the corresponding dimension
+   */
+  float getBoundaryPos(BoundaryEdge i_edge) {
+     if ( i_edge == BND_LEFT )
+       return (float)-5000;
+     else if ( i_edge == BND_RIGHT)
+       return (float)5000;
+     else if ( i_edge == BND_BOTTOM )
+       return (float)-5000;
+     else
+       return (float)5000;
+  };
 
 
 };
